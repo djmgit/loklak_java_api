0330c49442672fc7646c8615e56d9c688a2e0ea0
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2017-04-13 02:48:34 +0530

    Added methods to the api

--- Test.java
+++ Test.java
@@ -2,7 +2,8 @@ import loklak.java.api.*;
 class Test {
 	public static void main(String args[]) throws Exception {
 		Loklak l = new Loklak();
-		String r = l.search("linux", "", "", "", "");
+		//String r = l.aggregations("spacex", "2015-01-01", "", "mentions,hashtags", "2", "0");
+		String r = l.settings();
 		System.out.println(r);
 	}
 }
--- src/main/java/loklak/java/api/Loklak.java
+++ src/main/java/loklak/java/api/Loklak.java
@@ -14,6 +14,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
 
+import java.util.ArrayList;
+
 public class Loklak {
 
     private String baseUrl = "http://api.loklak.org/";
@@ -25,10 +27,12 @@ public class Loklak {
     private String until = "";
     private String source = "";
     private String count = "";
-    private String fields = "";
     private String fromUser = "";
     private String limit = "";
     private String action = "";
+    private String order = "";
+    private String orderBy = "";
+    private String fields;
 
     private final String USER_AGENT = "Mozilla/5.0";
 
@@ -36,6 +40,19 @@ public class Loklak {
 
 	public Loklak(String baseUrl) {
 		this.baseUrl = baseUrl;
+	}
+
+	public String buildReponse(HttpURLConnection con) throws IOException {
+        BufferedReader in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		return response.toString();
 	}
 
 	public String search(String query, String since, String until, String fromUser, String count) throws Exception {
@@ -85,16 +102,243 @@ public class Loklak {
 		}
 	}
 
-	public String buildReponse(HttpURLConnection con) throws IOException {
-        BufferedReader in = new BufferedReader(
-			        new InputStreamReader(con.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
+	public String suggest(String query, String count, String order, String orderBy, String since, String until)
+	                                                    throws Exception {
+		String apiUrl = "api/suggest.json?";
 
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-			return response.toString();
+		this.query = query;
+		this.count = count;
+		this.order = order;
+		this.orderBy = orderBy;
+		this.since = since;
+		this.until = until;
+
+		if (this.query != "") {
+			apiUrl = apiUrl + "query=" + this.query + "&";
 		}
+		if (this.count != "") {
+			apiUrl = apiUrl + "count=" + this.count + "&";
+		}
+		if (this.order != "") {
+			apiUrl = apiUrl + "order=" + this.order + "&";
+		}
+		if (this.orderBy != "") {
+			apiUrl = apiUrl + "orderBy=" + this.orderBy + "&";
+		} 
+		if (this.since != "") {
+			apiUrl = apiUrl + "since=" + this.since + "&";
+		}
+		if (this.until != "") {
+			apiUrl = apiUrl + "until=" + this.until + "&";
+		}
+
+		apiUrl = apiUrl.substring(0, apiUrl.length() - 1);
+		String url = this.baseUrl + apiUrl;
+
+		System.out.println(url);
+
+		URL urlObj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+
+        con.setRequestMethod("GET");
+
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        String response = "";
+		if (responseCode == 200) {
+            response = buildReponse(con);
+		} else {
+	        response = "{'error': 'Something went wrong, looks like the server is down.'}";
+		}
+
+		return response;
+ 	}
+
+ 	public String aggregations(String query, String since, String until, String fields, String limit, String count)
+ 	                                        throws Exception {
+
+ 		String apiUrl = "api/search.json?";
+
+ 		this.query = query;
+ 		this.since = since;
+ 		this.until = until;
+ 		this.fields = fields;
+ 		this.limit = limit == "" ? "6" : limit;
+ 		this.count = count == "" ? "0" : count;
+
+ 		if (query != "") {
+ 			apiUrl = apiUrl + "query=" + this.query;
+            if (since != "") {
+            	apiUrl = apiUrl + " since:" + this.since;
+            }
+            if (until != "") {
+            	apiUrl = apiUrl + " until:" + this.until;
+            }
+            apiUrl = apiUrl + "&source=cache";
+            apiUrl = apiUrl + "&count=" + this.count;
+            if (fields != "") {
+            	apiUrl = apiUrl + "&fields=" + this.fields;
+            }
+            apiUrl = apiUrl + "&limit=" + this.limit;
+
+            String url = this.baseUrl + apiUrl;
+            System.out.println(url);
+
+            URL urlObj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+
+            con.setRequestMethod("GET");
+
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            int responseCode = con.getResponseCode();
+            String response = "";
+			if (responseCode == 200) {
+                response = buildReponse(con);
+			} else {
+		        response = "{'error': 'Something went wrong, looks like the server is down.'}";
+			}
+
+			return response;
+ 		} else {
+			return "{'error': 'No Query string has been sent to query for an account'}";
+		}
+ 	}
+
+ 	public String status() throws Exception {
+ 		String apiUrl = "api/status.json";
+ 		
+ 		String url = this.baseUrl + apiUrl;
+        System.out.println(url); 
+
+        URL urlObj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+
+        con.setRequestMethod("GET");
+
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        String response = "";
+		if (responseCode == 200) {
+            response = buildReponse(con);
+		} else {
+	        response = "{'error': 'Something went wrong, looks like the server is down.'}";
+		}
+
+		return response;   
+ 	}
+
+ 	public String hello() throws Exception {
+ 		String apiUrl = "api/hello.json";
+ 		
+ 		String url = this.baseUrl + apiUrl;
+        System.out.println(url); 
+
+        URL urlObj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+
+        con.setRequestMethod("GET");
+
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        String response = "";
+		if (responseCode == 200) {
+            response = buildReponse(con);
+		} else {
+	        response = "{'error': 'Something went wrong, looks like the server is down.'}";
+		}
+
+		return response;   
+ 	}
+
+ 	public String peers() throws Exception {
+ 		String apiUrl = "api/peers.json";
+ 		
+ 		String url = this.baseUrl + apiUrl;
+        System.out.println(url); 
+
+        URL urlObj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+
+        con.setRequestMethod("GET");
+
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        String response = "";
+		if (responseCode == 200) {
+            response = buildReponse(con);
+		} else {
+	        response = "{'error': 'Something went wrong, looks like the server is down.'}";
+		}
+
+		return response;   
+ 	}
+
+ 	public String user(String name, String followers, String following) throws Exception {
+ 		String apiUrl = "api/user.json?";
+
+ 		this.name = name;
+ 		this.followers = followers;
+ 		this.following = following;
+
+ 		if (name != "") {
+ 			apiUrl = apiUrl + "screen_name=" + this.name;
+ 			if (followers != "") {
+ 				apiUrl = apiUrl + "&followers=" + this.followers;
+ 			}
+ 			if (following != "") {
+ 				apiUrl = apiUrl + "&following=" + this.following;
+ 			}
+
+ 			String url = this.baseUrl + apiUrl;
+ 			System.out.println(url);
+
+ 			URL urlObj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+
+            con.setRequestMethod("GET");
+
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            int responseCode = con.getResponseCode();
+            String response = "";
+			if (responseCode == 200) {
+                response = buildReponse(con);
+			} else {
+		        response = "{'error': 'Something went wrong, looks like the server is down.'}";
+			}
+
+			return response;
+ 		} else {
+ 			return "{'error': 'No user name given to query. Please check and try again'}";
+ 		}
+ 	}
+
+ 	public String settings() throws Exception {
+ 		String apiUrl = "api/settings.json";
+ 		
+ 		String url = this.baseUrl + apiUrl;
+        System.out.println(url); 
+
+        URL urlObj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+
+        con.setRequestMethod("GET");
+
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        String response = "";
+		if (responseCode == 200) {
+            response = buildReponse(con);
+		} else {
+	        response = "{'error': 'Something went wrong, looks like the server is down.'}";
+		}
+
+		return response;   
+ 	}
 }
