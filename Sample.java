ea4fdb6c7682cf46de1f0029e3ce4779be7aaa6d
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2017-04-14 11:39:31 +0530

    Updated Sample.java

--- Sample.java
+++ Sample.java
@@ -2,63 +2,105 @@ import loklak.java.api.*;
 import java.util.ArrayList;
 
 class Sample {
-	public static void main(String args[]) throws Exception {
-		Loklak loklak = new Loklak();
-		String response = "";
+    public static void main(String args[]) throws Exception {
+        Loklak loklak = new Loklak();
+        String response = "";
 
-		/* Loklak status API */
+        /* Loklak status API */
 
-		response = loklak.status();
-		System.out.println(response);
-		System.out.println("\n \n");
+        response = loklak.status();
+        System.out.println(response);
+        System.out.println("\n \n");
 
-		/* Loklak hello API */
+        /* Loklak hello API */
 
-		response = loklak.hello();
-		System.out.println(response);
-		System.out.println("\n \n");
+        response = loklak.hello();
+        System.out.println(response);
+        System.out.println("\n \n");
 
         /* Loklak settings API */
 
-		response = loklak.settings();
-		System.out.println(response);
-		System.out.println("\n \n");
+        response = loklak.settings();
+        System.out.println(response);
+        System.out.println("\n \n");
 
-		/* Loklak peers API */
+        /* Loklak peers API */
 
-		response = loklak.peers();
-		System.out.println(response);
-		System.out.println("\n \n");
+        response = loklak.peers();
+        System.out.println(response);
+        System.out.println("\n \n");
 
-		/* Loklak users API */
+        /* Loklak users API */
 
-		String userResponse1 = loklak.user("djm_dev96", "", "");
-		System.out.println(userResponse1);
-		System.out.println("\n \n");
+        String userResponse1 = loklak.user("djm_dev96", "", "");
+        System.out.println(userResponse1);
+        System.out.println("\n \n");
 
-		String userResponse2 = loklak.user("djm_dev96", "5", "7");
-		System.out.println(userResponse2);
-		System.out.println("\n \n");
+        String userResponse2 = loklak.user("djm_dev96", "5", "7");
+        System.out.println(userResponse2);
+        System.out.println("\n \n");
 
-		/* Susi API */
+        /* Susi API */
 
-		String susiResponse = loklak.susi("Hi");
-		System.out.println(susiResponse);
-		System.out.println("\n \n");
+        String susiResponse = loklak.susi("Hi");
+        System.out.println(susiResponse);
+        System.out.println("\n \n");
 
-		/* Loklak geocode API */
+        /* Loklak geocode API */
 
-		ArrayList<String> places = new ArrayList<String>();
-		places.add("Singapore");
-		places.add("Barcelona");
+        ArrayList<String> places = new ArrayList<String>();
+        places.add("Singapore");
+        places.add("Barcelona");
 
-		String geocodeResponse1 = loklak.geocode(places, false);
-		System.out.println(geocodeResponse1);
-		System.out.println("\n \n");
+        String geocodeResponse1 = loklak.geocode(places, false);
+        System.out.println(geocodeResponse1);
+        System.out.println("\n \n");
 
-		String geocodeResponse2 = loklak.geocode(places, true);
-		System.out.println(geocodeResponse2);
-		System.out.println("\n \n");
+        String geocodeResponse2 = loklak.geocode(places, true);
+        System.out.println(geocodeResponse2);
+        System.out.println("\n \n");
+
+        /* Loklak search API */
+
+        String searchResponse1 = loklak.search("linux", "", "", "", "");
+        System.out.println(searchResponse1);
+        System.out.println("\n \n");
+
+        String searchResponse2 = loklak.search("linux", "2016-06-01", "2017-03-34", "", "");
+        System.out.println(searchResponse2);
+        System.out.println("\n \n");
+
+        String searchResponse3 = loklak.search("linux", "", "", "", "5");
+        System.out.println(searchResponse3);
+        System.out.println("\n \n");
+
+        /*String searchResponse4 = loklak.search("fossasia", "", "", "fossasia", "5");
+        System.out.println(searchResponse4);
+        System.out.println("\n \n");*/
+
+        /* Loklak suggest API */
+
+        String suggestResponse1 = loklak.suggest("fossasia", "", "", "", "", "");
+        System.out.println(suggestResponse1);
+        System.out.println("\n \n");
+
+        String suggestResponse2 = loklak.suggest("fossasia", "5", "", "", "", "");
+        System.out.println(suggestResponse2);
+        System.out.println("\n \n");
+
+        String suggestResponse3 = loklak.suggest("fossasia", "5", "desc", "", "", "");
+        System.out.println(suggestResponse3);
+        System.out.println("\n \n");
+
+        String suggestResponse4 = loklak.suggest("fossasia", "5", "desc", "retrieval_next", "2014-01-01", "now");
+        System.out.println(suggestResponse4);
+        System.out.println("\n \n");
+
+        /* Loklak aggregation API */
+
+        String aggregationsResponse = loklak.aggregations("fossasia", "2014-01-01", "now", "hashtags,mentions", "10", "0");
+        System.out.println(aggregationsResponse);
+        System.out.println("\n \n");
 
     }
 }
